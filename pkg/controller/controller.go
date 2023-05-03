bab989d6a251a57769126929fc0345cf0edf1c7b
googs1025 <googs1025@gmail.com>
2023-05-03 12:01:43 +0800

    fix: 增加EventRecord事件通知器

--- pkg/controller/controller.go
+++ pkg/controller/controller.go
@@ -4,18 +4,27 @@ import (
 	"context"
 	inspectv1alpha1 "github.com/myoperator/inspectoperator/pkg/apis/inspect/v1alpha1"
 	"github.com/myoperator/inspectoperator/pkg/sysconfig"
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/client-go/tools/record"
 	"k8s.io/klog/v2"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
 	"sigs.k8s.io/controller-runtime/pkg/reconcile"
+	"sigs.k8s.io/controller-runtime/pkg/recorder"
+)
+
+var (
+	recorderProvider recorder.Provider
 )
 
 type InspectController struct {
 	client.Client
+	EventRecord record.EventRecorder
 }
 
 func NewInspectController() *InspectController {
-	return &InspectController{}
+	r := recorderProvider.GetEventRecorderFor("message-operator")
+	return &InspectController{EventRecord: r}
 }
 
 // Reconcile 调协loop
@@ -33,31 +42,36 @@ func (r *InspectController) Reconcile(ctx context.Context, req reconcile.Request
 	}
 	klog.Info(inspect)
 
-	// update config
-	err = sysconfig.AppConfig(inspect)
-	if err != nil {
-		return reconcile.Result{}, err
-	}
 
 	// 使用CreateOrUpdate处理业务逻辑
 	mutateInspectRes, err := controllerutil.CreateOrUpdate(ctx, r.Client, inspect, func() error {
+		// update config
+		err = sysconfig.AppConfig(inspect)
+		if err != nil {
+			r.EventRecord.Event(inspect, corev1.EventTypeWarning, "UpdateFail", "appconfig update fail...")
+			return err
+		}
+		r.EventRecord.Event(inspect, corev1.EventTypeNormal, "Update", "appconfig update...")
 		// FIXME: 如何解决重复进入的问题
 		klog.Info("is in...?")
 		// 业务逻辑
 		err = handleImage(&inspect.Spec)
 		if err != nil {
 			klog.Error("handle image error: ", err)
+			r.EventRecord.Event(inspect, corev1.EventTypeWarning, "RunImageTask", "running image inspect task error...")
 			return err
 		}
 		err = handleScript(&inspect.Spec)
 		if err != nil {
 			klog.Error("handle script error: ", err)
+			r.EventRecord.Event(inspect, corev1.EventTypeWarning, "RunScriptTask", "running script inspect task error...")
 			return err
 		}
+		r.EventRecord.Event(inspect, corev1.EventTypeNormal, "InspectTaskRunning", "running inspect task...")
 		return nil
 	})
 	if err != nil {
-		klog.Error("CreateOrUpdate error ", " Inspect ", err)
+		klog.Error("CreateOrUpdate error: ", err)
 		return reconcile.Result{}, err
 	}
 	klog.Info("CreateOrUpdate ", "Inspect ", mutateInspectRes)
@@ -65,7 +79,7 @@ func (r *InspectController) Reconcile(ctx context.Context, req reconcile.Request
 	return reconcile.Result{}, nil
 }
 
-// 使用controller-runtime 需要注入的client
+// InjectClient 使用controller-runtime 需要注入的client
 func (r *InspectController) InjectClient(c client.Client) error {
 	r.Client = c
 	return nil
